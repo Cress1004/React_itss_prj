d2b13137c615a4590aa19d876a65b6a07e9a93a8
nhat64283 <52846279+nhat64283@users.noreply.github.com>
2021-04-14 21:58:08 +0700

    fix wordlist

--- src/components/WordList.js
+++ src/components/WordList.js
@@ -3,9 +3,13 @@ import React, { useState } from 'react';
 function WordList({ item }) {
     //hiển thị danh sách những từ chưa checkbox ()chưa thuộc
   return (
-    <label className="panel-block">
+    <div>
+      {item.done?(<label className="panel-block">
         <p>{item.word} - {item.mean}</p>
-    </label>
+    </label>)
+    :("")}
+    </div>
+    
   );
 }
 
